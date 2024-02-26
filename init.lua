63e4f25bbe675a86655ff3f4baa935b298da4429
Carlos Augusto Monoo Pereira Barbosa <carlosmonoo@ifsp.edu.br>
2024-02-26 16:33:10 -0300

    Changed cmp next/previous binds

--- init.lua
+++ init.lua
@@ -662,7 +662,7 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ['<C-J>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_locally_jumpable() then
@@ -671,7 +671,7 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    ['<C-K>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.locally_jumpable(-1) then
